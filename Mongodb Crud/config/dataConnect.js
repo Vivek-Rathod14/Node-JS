3d0c72af528b4a705d0fd3b5853e9339394a3955
Vivek_Rathod14 <rathodvivek5500@gmail.com>
2026-01-27 21:22:12 +0530

    Delete Mongodb Crud directory

--- Mongodb Crud/config/dataConnect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const dbconnect = () => {
-    mongoose.connect("mongodb://localhost:27017/Student-Card")
-        .then(() => console.log("db is connet"))
-        .catch((err) => console.log(err));
-}
-module.exports = dbconnect;
