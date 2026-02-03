ea75789ae1e9e2bd9814f35d3f35a7129bb72c6c
Vivek_Rathod14 <rathodvivek5500@gmail.com>
2026-02-03 18:56:47 +0530

    pr

--- PR 3 Matrix/Server.js
+++ PR 3 Matrix/Server.js
@@ -1,24 +1,97 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+app.set("view engine", "ejs")
+app.use(express.static('public'))
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-app.get('/:page', (req, res) => {
-    res.render(req.params.page, (err, html) => {
-        if (err) {
-            res.status(404).send('Page not found');
-        } else {
-            res.send(html);
-        }
-    });
+app.get("/charts", (req, res) => {
+    res.render("charts");
 });
 
-app.listen(PORT, () => {
-    console.log('Server running on http://localhost:' + PORT);
+app.get("/widgets", (req, res) => {
+    res.render("widgets");
 });
+
+app.get("/tables", (req, res) => {
+    res.render("tables");
+});
+
+app.get("/grid", (req, res) => {
+    res.render("grid");
+});
+
+app.get("/form-basic", (req, res) => {
+    res.render("form-basic");
+});
+
+app.get("/form-wizard", (req, res) => {
+    res.render("form-wizard");
+});
+
+app.get("/pages-buttons", (req, res) => {
+    res.render("pages-buttons");
+});
+
+app.get("/icon-material", (req, res) => {
+    res.render("icon-material");
+});
+
+app.get("/icon-fontawesome", (req, res) => {
+    res.render("icon-fontawesome");
+});
+
+app.get("/pages-elements", (req, res) => {
+    res.render("pages-elements");
+});
+
+app.get("/index2", (req, res) => {
+    res.render("index2");
+});
+
+app.get("/pages-gallery", (req, res) => {
+    res.render("pages-gallery");
+});
+
+app.get("/pages-calendar", (req, res) => {
+    res.render("pages-calendar");
+});
+
+app.get("/pages-invoice", (req, res) => {
+    res.render("pages-invoice");
+});
+
+app.get("/pages-chat", (req, res) => {
+    res.render("pages-chat");
+});
+
+app.get("/authentication-login", (req, res) => {
+    res.render("authentication-login");
+});
+
+app.get("/authentication-register", (req, res) => {
+    res.render("authentication-register");
+});
+
+app.get("/error-403", (req, res) => {
+    res.render("/error-403");
+});
+
+app.get("/error-404", (req, res) => {
+    res.render("error-404");
+});
+
+app.get("/error-405", (req, res) => {
+    res.render("error-405");
+});
+
+app.get("/error-500", (req, res) => {
+    res.render("error-500");
+});
+app.listen(PORT, ()=>{
+    console.log(`Server started at port http://localhost:${PORT}`)
+})
